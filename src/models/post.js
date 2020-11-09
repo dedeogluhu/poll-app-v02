a1728c5191fd4155c3c493a5201a90d66c2756cc
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-11-09 20:51:43 +0300

    Update post.js schema

--- src/models/post.js
+++ src/models/post.js
@@ -3,9 +3,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
-const Post = new Schema({
-    author: ObjectId,
+const PostSchema = new Schema({
+    id: ObjectId,
     header: String,
     choices: Array,
     date: Date
 });
+
+module.exports = mongoose.model('Post', PostSchema);
