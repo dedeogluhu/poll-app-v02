25ea51f1608f4f3f55da2aa10a4ca7c1eeb3805f
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-11-10 20:26:42 +0300

    Update connectMongoose function at indexRouter

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,21 +4,19 @@ var express = require('express');
 var router = express.Router();
 
 async function connectMongoose() {
-  try {
-    await mongoose.connect(mongoUri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true
-    });
-
-    console.log('MongoDb Connected');
-  } catch (error) {
-    console.log(error);
-  }
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  })
+    .then(console.log('MongoDb Connected'))
+    .catch(error => console.log(error));
 }
 
-//connectMongoose();
+connectMongoose();
+
+
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
