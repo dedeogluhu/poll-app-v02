2a13d6a803161f9330bb7ab384018c33c24e701d
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-11-10 19:32:00 +0300

    Add connect mongodb function to index router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,27 @@
+const mongoose = require('mongoose');
+const { mongoUri } = require('../config');
 var express = require('express');
 var router = express.Router();
 
+async function connectMongoose() {
+  try {
+    await mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true
+    });
+
+    console.log('MongoDb Connected');
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//connectMongoose();
+
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index');
 });
 
