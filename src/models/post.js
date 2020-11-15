ab607b57f32b23c871a9fd84975e603f5e8055bd
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-11-15 20:50:48 +0300

    Update post model

--- src/models/post.js
+++ src/models/post.js
@@ -1,13 +1,9 @@
 const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
-
-const PostSchema = new Schema({
-    id: ObjectId,
-    header: { type: String, required: true },
+const PostSchema = mongoose.Schema({
+    question: { type: String, required: true },
     choices: { type: [String], required: true },
-    date: { type: Date, required: true }
+    date: { type: Date, required: true, default: Date.now() }
 });
 
 module.exports = mongoose.model('Post', PostSchema);
