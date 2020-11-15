4b3eaa05cb9a01635d32d0a96222cd3c09d79875
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-11-15 20:50:34 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-    mongoUri: process.env.MONGO_URI,
-    port: process.env.PORT || '8800'
+    MONGO_URI: process.env.MONGO_URI,
+    PORT: process.env.PORT || '8800'
 };
