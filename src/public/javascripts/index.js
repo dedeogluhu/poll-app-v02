2f89a85ae62ba35fdf266ed597814b43be18b345
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-11-29 23:29:45 +0300

    Implement getting posts from database

--- src/public/javascripts/index.js
+++ src/public/javascripts/index.js
@@ -1,94 +1,94 @@
-let examplePost = {
-    header: '1-) Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, nemo? Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.',
-    choices: [
-        'First choice',
-        'Second choice',
-        'Third choice',
-        'Fourth choice'
-    ],
-    date: new Date(2018, 11, 24, 10, 33, 30, 0)
-};
+// let examplePost = {
+//     header: '1-) Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, nemo? Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.',
+//     choices: [
+//         'First choice',
+//         'Second choice',
+//         'Third choice',
+//         'Fourth choice'
+//     ],
+//     date: new Date(2018, 11, 24, 10, 33, 30, 0)
+// };
+// let examplePost2 = {
+//     header: '2-) Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, nemo? Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.',
+//     choices: [
+//         'First choice2',
+//         'Second choice2',
+//         'Third choice2',
+//         'Fourth choice2'
+//     ],
+//     date: new Date(2017, 11, 24, 10, 33, 30, 0)
+// };
+// let examplePostList = [
+//     examplePost,
+//     examplePost2,
+// ];
+// examplePostList.sort((x, y) => {
+//     return y.date - x.date;
+// });
 
-let examplePost2 = {
-    header: '2-) Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, nemo? Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.',
-    choices: [
-        'First choice2',
-        'Second choice2',
-        'Third choice2',
-        'Fourth choice2'
-    ],
-    date: new Date(2017, 11, 24, 10, 33, 30, 0)
-};
+const postListDiv = document.querySelector('#post-list');
+let postList = [];
+let postListHtml = '';
 
-let examplePost3 = {
-    header: '3-) Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, nemo? Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.',
-    choices: [
-        'First choice3',
-        'Second choice3',
-        'Third choice3',
-        'Fourth choice3'
-    ],
-    date: new Date(2019, 11, 24, 10, 33, 30, 0)
-};
-
-let examplePost4 = {
-    header: '4-) Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, nemo? Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.',
-    choices: [
-        'First choice4',
-        'Second choice4',
-        'Third choice4',
-        'Fourth choice4',
-        'Fifth choice4',
-        'Sixth choice4',
-    ],
-    date: Date.now()
-};
-
-let examplePostList = [
-    examplePost,
-    examplePost2,
-    examplePost3,
-    examplePost4
-];
-
-examplePostList.sort((x, y) => {
-    return y.date - x.date;
-});
-
-let postList = '';
-
-for (let example of examplePostList) {
-    let choices = '';
-
-    let exampleChoices = example.choices;
-
-    if (exampleChoices.length > 4) {
-        let extraCount = exampleChoices.length - 4
-        exampleChoices[3] += ` and ${extraCount} more`;
-    }
-
-    for (let i = 0; i < 4; i++) {
-        let choiceTemplate = `
-            <button class="choice">
-                <span class='choice-sign'>X</span>
-                ${exampleChoices[i]}
-            </button>
-        `;
-        choices += choiceTemplate;
-    }
-
-    let post = `
-    <div class="post">
-        <p class="post-header">${example.header}</p>
-        <div class="choice-list">
-            ${choices}
-        </div>
-    </div>
-`;
-
-    postList += post;
+async function main() {
+    await getPosts();
+    setPosts(postList);
 }
 
+async function getPosts() {
+    await axios.get('/api/posts')
+        .then(function (response) {
+            for (let element of response.data) {
+                postList.push(element);
+                console.log(element);
+            }
+        })
+        .catch(error => console.log(error));
+}
 
+function sortPosts(list) {
+    list.sort((x, y) => {
+        return y.date - x.date;
+    });
+}
 
-document.querySelector('#post-list').innerHTML = postList;
+function setPosts(list) {
+
+    sortPosts(list);
+
+    for (let post of list) {
+        let choicesHtml = '';
+
+        let postChoices = post.choices;
+
+        if (postChoices.length > 4) {
+            let extraCount = postChoices.length - 4
+            postChoices[3] += ` and ${extraCount} more`;
+        }
+
+        for (let i = 0; i < 4; i++) {
+            let choiceTemplate = `
+            <button class="choice">
+            <span class='choice-sign'>X</span>
+            ${postChoices[i]}
+            </button>
+            `;
+            choicesHtml += choiceTemplate;
+        }
+
+        let postHtml = `
+        <div class="post">
+        <p class="post-header">${post.question}</p>
+        <div class="choice-list">
+        ${choicesHtml}
+        </div>
+        </div>
+        `;
+
+        postListHtml += postHtml;
+    }
+
+    postListDiv.innerHTML = postListHtml;
+}
+
+main();
